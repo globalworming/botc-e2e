b13528394c945bc254ac12e1bc2d3634e44e4556
wormi <andreas.worm@headissue.com>
2020-05-10 09:00:59 +0200

    run parallel, wait for sut

---
+++ .circleci/waitForSutReady.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+stopAfterTries=20
+currentTry=0
+
+pollUntilOkResponse() {
+  port=$1
+  url="http://localhost:${port}"
+  echo "waiting for ${url}"
+  until $(curl --output /dev/null --silent --head --fail ${url}); do
+      countTries
+      printf "."
+      sleep 5
+  done
+}
+
+countTries() {
+  currentTry=$((currentTry+1))
+  if [ ${currentTry} -ge ${stopAfterTries} ]; then
+    echo "gave up"
+    exit 1
+  fi
+}
+
+
+pollUntilOkResponse 3000
+pollUntilOkResponse 8080
+
+$*
