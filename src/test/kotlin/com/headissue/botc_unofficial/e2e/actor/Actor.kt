7e7cfc1ae94a38f1883f657bc28eead7b2ea43f9
wormi <andreas.worm@headissue.com>
2020-05-14 15:22:15 +0200

    containerize an executable application

---
+++ src/test/kotlin/com/headissue/botc_unofficial/e2e/actor/Actor.kt
@@ -0,0 +1,19 @@
+package com.headissue.botc_unofficial.e2e.actor
+/*
+import net.serenitybdd.screenplay.Actor
+
+fun Actor.role():ActorRole=if(abilityTo(UseAMobileDevice::class.java)!=null){
+  ActorRole.MobileActor
+}else{
+  ActorRole.ApiActor
+}
+
+/*
+actor.assumingTheyCan(BrowseTheWeb.class).attemptsTo(
+  Open.url(url), Click.on(otherTarget)
+)
+
+actor.assumingTheyCan(CallRestApi.class).attemptsTo(
+  Post(..)
+)
+ */
