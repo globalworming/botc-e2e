03e57bef7013b00b142e21a116bbe311c44f7633
wormi <andreas.worm@headissue.com>
2020-05-16 21:34:29 +0200

    change template

--- src/test/kotlin/com/headissue/botc_unofficial/e2e/action/ActionTemplate.kt
+++ src/test/kotlin/com/headissue/botc_unofficial/e2e/action/ActionTemplate.kt
@@ -1,15 +1,19 @@
 package com.headissue.botc_unofficial.e2e.action
 
+import com.headissue.botc_unofficial.e2e.ability.AccessLocalFrontendMockGameTable
+import com.headissue.botc_unofficial.e2e.ability.AccessLocalIntegratedFrontend
 import com.headissue.botc_unofficial.e2e.ability.AccessLocalRestAPI
 import net.serenitybdd.screenplay.Actor
 import net.serenitybdd.screenplay.NoMatchingAbilityException
 import net.serenitybdd.screenplay.Performable
-import net.serenitybdd.screenplay.abilities.BrowseTheWeb
-import net.serenitybdd.screenplay.actions.Click
 
 open class ActionTemplate : Performable {
   override fun <T : Actor> performAs(actor: T) {
-    if (actor.abilityTo(BrowseTheWeb::class.java) != null) {
+    if (actor.abilityTo(AccessLocalFrontendMockGameTable::class.java) != null) {
+      return
+    }
+
+    if (actor.abilityTo(AccessLocalIntegratedFrontend::class.java) != null) {
       return
     }
 
